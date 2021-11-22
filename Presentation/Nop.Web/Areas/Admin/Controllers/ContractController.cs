a187382848beb834f8d56e1103a65aa37a187984
sohel-ekt <Asus@DESKTOP-CVSU4IE>
2021-11-23 03:56:16 +0600

    contact fixed

--- Presentation/Nop.Web/Areas/Admin/Controllers/ContractController.cs
+++ Presentation/Nop.Web/Areas/Admin/Controllers/ContractController.cs
@@ -1,10 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Nop.Services.Contacts;
+﻿
 using Nop.Web.Models.Contact;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Topics;
+using Nop.Services.Common;
+using Nop.Services.Customers;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Messages;
+using Nop.Services.Security;
+using Nop.Services.Seo;
+using Nop.Services.Stores;
+using Nop.Services.Topics;
+using Nop.Web.Areas.Admin.Factories;
+using Nop.Web.Areas.Admin.Infrastructure.Mapper.Extensions;
+using Nop.Web.Areas.Admin.Models.Topics;
+using Nop.Web.Framework.Mvc.Filters;
+using Nop.Services.Contacts;
 
 namespace Nop.Web.Areas.Admin.Controllers
 {
@@ -13,21 +28,81 @@ namespace Nop.Web.Areas.Admin.Controllers
         #region Fields
 
         private readonly IContactService _contactService;
+        private readonly IAclService _aclService;
+        private readonly ICustomerActivityService _customerActivityService;
+        private readonly ICustomerService _customerService;
+        private readonly ILocalizationService _localizationService;
+        private readonly ILocalizedEntityService _localizedEntityService;
+        private readonly INotificationService _notificationService;
+        private readonly IPermissionService _permissionService;
+        private readonly IStoreMappingService _storeMappingService;
+        private readonly IStoreService _storeService;
+        private readonly ITopicModelFactory _topicModelFactory;
+        private readonly ITopicService _topicService;
+        private readonly IUrlRecordService _urlRecordService;
+        private readonly IGenericAttributeService _genericAttributeService;
+        private readonly IWorkContext _workContext;
+
+
+
         //private readonly IContactModelFactory _contactModelFactory;
 
         #endregion
 
 
-        public ContractController(IContactService contactService
-            //IContactModelFactory contactModelFactory
+        public ContractController(IContactService contactService,
+           IAclService aclService,
+            ICustomerActivityService customerActivityService,
+            ICustomerService customerService,
+            ILocalizationService localizationService,
+            ILocalizedEntityService localizedEntityService,
+            INotificationService notificationService,
+            IPermissionService permissionService,
+            IStoreMappingService storeMappingService,
+            IStoreService storeService,
+            ITopicModelFactory topicModelFactory,
+            ITopicService topicService,
+            IUrlRecordService urlRecordService,
+            IGenericAttributeService genericAttributeService,
+            IWorkContext workContext
             )
         {
             this._contactService = contactService;
-            //this._contactModelFactory = contactModelFactory;
+            _aclService = aclService;
+            _customerActivityService = customerActivityService;
+            _customerService = customerService;
+            _localizationService = localizationService;
+            _localizedEntityService = localizedEntityService;
+            _notificationService = notificationService;
+            _permissionService = permissionService;
+            _storeMappingService = storeMappingService;
+            _storeService = storeService;
+            _topicModelFactory = topicModelFactory;
+            _topicService = topicService;
+            _urlRecordService = urlRecordService;
+            _genericAttributeService = genericAttributeService;
+            _workContext = workContext;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(bool showtour = false)
         {
-            return View();
+            if (!await _permissionService.AuthorizeAsync(StandardPermissionProvider.ManageTopics))
+                return AccessDeniedView();
+
+            //prepare model
+            var model = await _topicModelFactory.PrepareTopicSearchModelAsync(new TopicSearchModel());
+
+            //show configuration tour
+            if (showtour)
+            {
+                var hideCard = await _genericAttributeService.GetAttributeAsync<bool>(await _workContext.GetCurrentCustomerAsync(), NopCustomerDefaults.HideConfigurationStepsAttribute);
+
+                var closeCard = await _genericAttributeService.GetAttributeAsync<bool>(await _workContext.GetCurrentCustomerAsync(), NopCustomerDefaults.CloseConfigurationStepsAttribute);
+
+                if (!hideCard && !closeCard)
+                    ViewBag.ShowTour = true;
+            }
+
+            return View(model);
         }
         [HttpPost]
         /// <returns>A task that represents the asynchronous operation</returns>
---
+++ Presentation/Nop.Web/Areas/Admin/Models/Contact/ContactSearchModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nop.Web.Areas.Admin.Models.Contact
+{
+    public partial record ContactSearchModel : BaseSearchModel
+    {
+
+        public ContactSearchModel()
+        {
+            AvailableStores = new List<SelectListItem>();
+        }
+
+        [NopResourceDisplayName("Admin.ContentManagement.Topics.List.SearchStore")]
+        public int SearchStoreId { get; set; }
+
+        public IList<SelectListItem> AvailableStores { get; set; }
+
+        [NopResourceDisplayName("Admin.ContentManagement.Topics.List.SearchKeywords")]
+        public string SearchKeywords { get; set; }
+
+        public bool HideStoresList { get; set; }
+    }
+}
